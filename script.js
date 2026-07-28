adf86251995dec45b09d646189eca0b72529009c
Dylan waweru <dylanwaweru.000@gmail.com>
2026-07-28 11:34:26 +0300

    Create Akan name generator with HTML CSS and JavaScript

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const akanNames = {
   male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
   female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 };
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
 
 const form = document.getElementById("akan-form");
 const resultSection = document.getElementById("result");
